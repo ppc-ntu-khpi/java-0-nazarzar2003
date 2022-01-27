bd682f825982eff11db7a4fa4089be3d3402a93b
nazarzar2003 <41821678+nazarzar2003@users.noreply.github.com>
2022-01-27 07:54:39 +0000

    done

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,10 +1,10 @@
 public class Shirt {
-  public int shirtID = 0; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
+  public int shirtID = 5; // стандартне значення номера моделі сорочки
+  public String description = "-very comfortable-"; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'U';
-  public double price = 0.0; // стандартна вартість сорочки
-  public int quantityInStock = 0; // стандартна кількість на складі
+  public String colorCode = "Green";
+  public double price = 7.0; // стандартна вартість сорочки
+  public int quantityInStock = 9; // стандартна кількість на складі
   
   // цей метод просто виводить всю інформацію про сорочку на екран
   public void displayShirtInformation() {
